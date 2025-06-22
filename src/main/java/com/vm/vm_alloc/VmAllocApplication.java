c30027bb867828d4ea2cf5f6b8d1aa5a195f2273
ibnul-nabil <2005060@ugrad.cse.buet.ac.bd>
2025-06-22 16:37:19 +0600

    1st commit

---
+++ src/main/java/com/vm/vm_alloc/VmAllocApplication.java
@@ -0,0 +1,13 @@
+package com.vm.vm_alloc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VmAllocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VmAllocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vm/vm_alloc/VmAllocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vm.vm_alloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VmAllocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
